2248131416a477510d42719955cf5e2760308d99
arisromil <arisromil@yahoo.com>
2023-07-21 00:03:55 -0600

    repo of new-ish patterns and building with gradle

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+
+    kotlin("jvm") version "1.9.0";
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    // implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.0")
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.5.2")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
