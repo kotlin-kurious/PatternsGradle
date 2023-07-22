bff43e88f90ac1a515eb22ee99e738001fc3b74e
arisromil <arisromil@yahoo.com>
2023-07-22 00:17:46 -0600

    barrier for homogenous asynchronous results
    When working with a set of homogenous asynchronous results and you need all of them to complete before proceeding further, use awaitAll(), which acts as a barrier as well

---
+++ src/main/kotlin/barriers/HomogenousBarrier.kt
@@ -0,0 +1,48 @@
+package barriers
+
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlin.random.Random
+
+class HomogenousBarrier {
+    suspend fun main() {
+        val characters: List<Deferred<FavoriteCharacter>> =
+            listOf(
+                Homer .getFavoriteCharacter(),
+                Chandler.getFavoriteCharacter(),
+                Rachel.getFavoriteCharacter(),
+            )
+        println(characters.awaitAll());
+    }
+}
+
+data class FavoriteCharacter(
+    val name: String,
+    val catchphrase: String,
+    val picture: ByteArray = Random.nextBytes(42)
+)
+object Rachel {
+    suspend fun getFavoriteCharacter() = coroutineScope {
+        async {
+            FavoriteCharacter("Ross", "We were on a break")
+        }
+    }
+}
+
+object Chandler {
+    suspend fun getFavoriteCharacter() = coroutineScope {
+        async {
+            FavoriteCharacter("Janice", "Oh my God")
+        }
+    }
+}
+
+object Homer  {
+    suspend fun getFavoriteCharacter() = coroutineScope {
+        async {
+            FavoriteCharacter("Homer", "doh!")
+        }
+    }
+}
