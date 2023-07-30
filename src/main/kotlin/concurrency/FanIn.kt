d5ef664505b8fb465b5c28facb2c400218bbb95a
arisromil <arisromil@yahoo.com>
2023-07-30 00:26:42 -0600

    Fan-In pattern to combine results from multiple workers. useful when workers produce results and need to gather them

---
+++ src/main/kotlin/concurrency/FanIn.kt
@@ -0,0 +1,34 @@
+package concurrency
+
+import generateWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        val workChannel = generateWork()
+        val resultChannel = Channel<String>()
+        val workers = List(10) {
+            doWorkAsync(workChannel, resultChannel)
+        }
+
+        resultChannel.consumeEach {
+            println(it)
+        }
+    }
+}
+
+
+private fun CoroutineScope.doWorkAsync(
+    channel: ReceiveChannel<String>,
+    resultChannel: Channel<String>
+) = async(Dispatchers.Default) {
+    for (p in channel) {
+        resultChannel.send(p.repeat(2))
+    }
+}
